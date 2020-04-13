1e44089124858aab94da7d0ec1ba13e70228ea77
CananArikan <arikancnn@gmail.com>
2020-04-13 19:06:25 -0400

    added AbstractTestBase class

--- src/test/java/com/automation/utilities/BrowserUtils.java
+++ src/test/java/com/automation/utilities/BrowserUtils.java
@@ -1,11 +1,17 @@
 package com.automation.utilities;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class BrowserUtils {
@@ -53,4 +59,31 @@ public class BrowserUtils {
             error.printStackTrace();
         }
     }
+
+    /**
+     * Take screenshot of page
+     * @param name
+     * @return
+     */
+    public static String getScreenshot(String name) {
+        name = new Date().toString().replace(" ", "_").replace(":", "-") + "_" + name;
+        String path = "";
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            path = System.getProperty("user.dir") + "/test-output/screenshots/" + name + ".png";
+        } else {
+            path = System.getProperty("user.dir") + "\\test-output\\screenshots\\" + name + ".png";
+        }
+        System.out.println("OS name: " + System.getProperty("os.name"));
+        System.out.println("Screenshot is here: " + path);
+        TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+        File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File destination = new File(path);
+        try {
+            FileUtils.copyFile(source, destination);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return path;
+    }
+
 }
